fb72f61ff7ea8cdc2d0e44e5f3a8bf0f4db24f13
IanCostaa <163686445+IanCostaa@users.noreply.github.com>
2025-03-21 15:53:38 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,76 +1,48 @@
-import java.nio.charset.UnsupportedCharsetException;
 import java.util.Scanner;
 
 public class Main{
 
-    public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
-        
-        System.out.println("Digite o numero de rounds: ");
-        int n = scan.nextInt();
-        int userWins = 0;
-        int computerWins = 0;
-        int Empates = 0;
-        
-         System.out.println("O valor de n é: "+ n);
+	public static void main(String[] args){
+		
+		int op = 0;
+		Scanner scan = new Scanner(System.in);
+		int saldo = 0;
+		int saque =0;
+		
+		
+		do{
+	    	System.out.println("Selecione a opçõe:");
+    		System.out.println("1 - Ver saldo");
+    		System.out.println("2 - Depósito");
+    		System.out.println("3 - Saque");
+    		System.out.println("0 - Sair");
+    		op = scan.nextInt();
+    		
+    		switch(op){
+    		    case 1:
+    		        System.out.printf("\n saldo:" + saldo + "\n");
+    		        break;
+    		        
+    		    case 2:
+    		        System.out.println("Digite o valor que deseja depositar:");
+    		        saldo = scan.nextInt();
+    		        break;
+    		   
+    		    case 3:
+    		        System.out.println("Digite o valor que deseja sacar:");
+    		        saque = scan.nextInt();
+    		        
+    		        saldo = saldo - saque;
+    		       
+    		        System.out.println("Saldo: " + saldo);
+    		        
+    		        break;
+    		}
+    		
+		}while(op != 0 );
+		
+		scan.close();
 
-        for(int i = 0;i < n;i++){
-            System.out.println("1- Pedra, 2- Papel, 3- Tesoura");
-            int userChoice = scan.nextInt();
-            String userChoiceString = getCastChoice(userChoice);
-
-            int computerChoice = (int)(Math.random()* 3) + 1;
-            String computerChoiceString = getCastChoice(computerChoice);
-
-            System.out.println("Você escolheu " + userChoiceString + " e o computador escolheu " + computerChoiceString);
-
-            
-            if(computerChoice == 1 && userChoice == 3){
-                System.out.println("O computador ganhou está rodada");
-                computerWins++;
-            }
-            else if(computerChoice == 2 && userChoice == 1){
-                System.out.println("O computador ganhou está rodada");
-                computerWins++;
-            }
-            else if(computerChoice == 3 && userChoice == 2){
-                System.out.println("O computador ganhou está rodada");
-                computerWins++;
-            }
-            else if(computerChoice == userChoice){
-                System.out.println("Empatou está rodada");
-                Empates++;
-            }
-            else{
-                System.out.println("O usuario ganhou está rodada");
-                userWins++;
-            }
-        }
-
-        if(computerWins > userWins){
-            System.out.println("O computador ganhou");
-        }
-        else if(computerWins < userWins){
-            System.out.println("O usuario ganhou");
-        }
-        else{
-            System.out.println("Empate");
-        }
-        System.out.printf("Usuário %d x %d Computador | %d Empates ", userWins, computerWins, Empates);
-
-        scan.close();
-    }
+}
     
-    public static String getCastChoice(int choice){
-        switch(choice){
-            case 1:
-                return "Pedra";
-            case 2:
-                return "Papel";
-            case 3:
-                return "Tesoura";
-            default:
-                return "Opção inválida";
-            }
-        }
 }
