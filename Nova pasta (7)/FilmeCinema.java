e711273adceb08dec9bd6bf24ed98f713a07485f
IanCostaa <163686445+IanCostaa@users.noreply.github.com>
2025-06-11 09:33:01 -0300

    Add files via upload

---
+++ Nova pasta (7)/FilmeCinema.java
@@ -0,0 +1,9 @@
+public class FilmeCinema extends Filme {
+
+    private Integer duracaoMinutos;
+
+    public FilmeCinema(Diretor diretor, Integer anoLancamento, String titulo) {
+        super(diretor, anoLancamento, titulo);
+    }
+
+}
