758b211c83090975cbc6f6f8895821229cfb2cde
IanCostaa <163686445+IanCostaa@users.noreply.github.com>
2025-06-11 10:53:55 -0300

    Delete Nova pasta (7) directory

--- Nova pasta (7)/FilmeCinema.java
+++
@@ -1,9 +0,0 @@
-public class FilmeCinema extends Filme {
-
-    private Integer duracaoMinutos;
-
-    public FilmeCinema(Diretor diretor, Integer anoLancamento, String titulo) {
-        super(diretor, anoLancamento, titulo);
-    }
-
-}
