08ac7b46baea32863ee4dc2aa9ba9b39aefdeedd
IanCostaa <163686445+IanCostaa@users.noreply.github.com>
2025-06-13 14:01:07 -0300

    atualização no codigo do filme

--- Filme/Main.java
+++ Filme/Main.java
@@ -1,13 +1,21 @@
 package Filme;
+import java.util.Scanner;
 
 public class Main {
     
     Diretor diretor1 = new Diretor("José" , "Brasil");
     Diretor diretor2 = new Diretor("Mohamed" , "Mexico");
 
-    FilmeCinema filmeCinema1 = new FilmeCinema(diretor1, 2000, "as longas tranças de um careca");
+    FilmeCinema filmeCinema1 = new FilmeCinema(diretor1, 2000, "As longas tranças de um careca");
     FilmeCinema filmeCinema2 = new FilmeCinema(diretor2, 1970, "Branca de neve e os 7 negões");
 
-    FilmeStreaming filmeStreaming1 = new FilmeStreaming(diretor1, 2000, "as longas tranças de um careca", "FilmeFlix");
-    FilmeStreaming filmeStreaming2 = new FilmeStreaming(direotr2 , 1970, "Branca de neve e os 7 negões", "xfilmes");
+    FilmeStreaming filmeStreaming1 = new FilmeStreaming();
+    FilmeStreaming filmeStreaming2 = new FilmeStreaming();
+
+    Array<Filme> colecaoFilme = new Arraylist<>();
+
+    colecaoFilme.add(filmeCinema1);
+    colecaoFilme.add(filmeCinema2);
+    colecaoFilme.add(filmeStreaming1);
+    colecaoFilme.add(filmeStreaming2);
 }
