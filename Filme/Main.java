824cc33946f9b538520de79a98ab37e4e04e1e6b
IanCostaa <163686445+IanCostaa@users.noreply.github.com>
2025-06-25 15:18:47 -0300

    Atualizar o Main.java

--- Filme/Main.java
+++ Filme/Main.java
@@ -1,63 +1,70 @@
 package Filme;
-import java.util.Scanner;
+
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
-    public Integer opçao;
-    Private String nomeBusca;
+    public static void main(String[] args) {
 
-    for(;;){
-        switch (opçao){
-            case 1:
-                Scanner scanner = new Scanner(System.in);
-                System.out.printl("Digite o nome do diretor:");
-                nomeBusca = scanner.nextline();
-                filtrarPorDiretor(colecaoFilme , nomeBusca);
-        };
-       
+        Scanner scanner = new Scanner(System.in);
+
+        Diretor diretor1 = new Diretor("José", "Brasil");
+        Diretor diretor2 = new Diretor("Mohamed", "Mexico");
+
+        FilmeCinema filmeCinema1 = new FilmeCinema(diretor1, 2000, "Carros5");
+        FilmeCinema filmeCinema2 = new FilmeCinema(diretor2, 1970, "Wally");
+
+        FilmeStreaming filmeStreaming1 = new FilmeStreaming(new Diretor("Ana", "Argentina"), 2020, "Digital 1", "Netflix");
+        FilmeStreaming filmeStreaming2 = new FilmeStreaming(new Diretor("Carlos", "Chile"), 2022, "Digital 2", "Amazon");
+
+        ArrayList<Filme> colecaoFilme = new ArrayList<>();
+
+        colecaoFilme.add(filmeCinema1);
+        colecaoFilme.add(filmeCinema2);
+        colecaoFilme.add(filmeStreaming1);
+        colecaoFilme.add(filmeStreaming2);
+
+        while (true) {
+            exibirOpcoes();
+            System.out.print("Escolha uma opção: ");
+            int opcao = scanner.nextInt();
+            scanner.nextLine(); // limpar o buffer
+
+            switch (opcao) {
+                case 1:
+                    System.out.print("Digite o nome do diretor: ");
+                    String nomeBusca = scanner.nextLine();
+                    filtrarPorDiretor(colecaoFilme, nomeBusca);
+                    break;
+                case 0:
+                    System.out.println("Encerrando programa...");
+                    return;
+                default:
+                    System.out.println("Opção inválida.");
+            }
+        }
     }
-    
-    Diretor diretor1 = new Diretor("José" , "Brasil");
-    Diretor diretor2 = new Diretor("Mohamed" , "Mexico");
 
-    FilmeCinema filmeCinema1 = new FilmeCinema(diretor1, 2000, "Carros5");
-    FilmeCinema filmeCinema2 = new FilmeCinema(diretor2, 1970, "Wally");
+    public static void filtrarPorDiretor(ArrayList<Filme> lista, String nomeDiretor) {
+        boolean encontrou = false;
 
-    FilmeStreaming filmeStreaming1 = new FilmeStreaming();
-    FilmeStreaming filmeStreaming2 = new FilmeStreaming();
-
-    Array<Filme> colecaoFilme = new Arraylist<>();
-
-    colecaoFilme.add(filmeCinema1);
-    colecaoFilme.add(filmeCinema2);
-    colecaoFilme.add(filmeStreaming1);
-    colecaoFilme.add(filmeStreaming2);
-
-    
-}
-public static void filtrarPorDiretor(Array<Filme> String plataformaDesejada){
-     
-     private boolean encontrou = false;
-
-     for(filme Filme : lista){
-        if(filme.getDiretor().getNome() equalsIgnoreCase(nomeDiretor)){
-            filmes.exibirDetalhe();
-            System.out.println();
-            encontrou = true;
+        for (Filme filme : lista) {
+            if (filme.getDiretor().getNome().equalsIgnoreCase(nomeDiretor)) {
+                filme.exibirDetalhe();
+                System.out.println();
+                encontrou = true;
+            }
         }
-        if(!encontrou){
-            Syestem.out.printl("Nenhum filme encontrado com esse diretor " + nomeDiretor);
+
+        if (!encontrou) {
+            System.out.println("Nenhum filme encontrado com esse diretor: " + nomeDiretor);
         }
-     }
+    }
 
-
-}
-public static void exibirOpções(){
-
-    Syestem.out.println(------------------------------------------------------);
-    Syestem.out.println("1- Buscar filme por diretor");
-    Syestem.out.println("2- Buscar por ano de lançamento");
-    System.out.println("3- Buscar por Por plataforma");
-    
+    public static void exibirOpcoes() {
+        System.out.println("------------------------------------------------------");
+        System.out.println("1- Buscar filme por diretor");
+        System.out.println("0- Sair");
+    }
 }
